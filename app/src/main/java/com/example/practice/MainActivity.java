0978907804c7d191eb9c5ee1d585389c525d9645
MuHongWeiWei <4a490094@stust.edu.tw>
2019-02-21 11:48:09 +0800

    SharedPreferences practice

--- app/src/main/java/com/example/practice/MainActivity.java
+++ app/src/main/java/com/example/practice/MainActivity.java
@@ -1,62 +1,59 @@
 package com.example.practice;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.util.Map;
-
 public class MainActivity extends AppCompatActivity {
+
 
     private EditText etName;
     private EditText etPassword;
-    private CheckBox cbIsCheck;
+    private CheckBox isCheck;
+    private SharedPreferences infoSp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        infoSp = getSharedPreferences("info", MODE_PRIVATE);
+
         etName = findViewById(R.id.et_name);
         etPassword = findViewById(R.id.et_password);
-        cbIsCheck = findViewById(R.id.cb_isCheck);
+        isCheck = findViewById(R.id.cb_isCheck);
 
-        Map<String, String> maps = TextInfoUtils.readInfo(this);
-
-        String name = maps.get("name");
-        String password = maps.get("password");
+        String name = infoSp.getString("name", "");
+        String password = infoSp.getString("password", "");
+        boolean check = infoSp.getBoolean("check", false);
 
         etName.setText(name);
         etPassword.setText(password);
+        isCheck.setChecked(check);
 
     }
 
 
     public void login(View view) {
-        String name = etName.getText().toString();
-        String password = etPassword.getText().toString();
+        String name = etName.getText().toString().trim();
+        String password = etPassword.getText().toString().trim();
 
-        if (cbIsCheck.isChecked()) {
-            boolean succes = TextInfoUtils.saveInfo(MainActivity.this, name, password);
-            if (succes) {
-                Toast.makeText(MainActivity.this, "保存成功", Toast.LENGTH_LONG).show();
-            } else {
-                Toast.makeText(MainActivity.this, "保存失敗", Toast.LENGTH_LONG).show();
-            }
-        } else {
-            Toast.makeText(MainActivity.this, "請勾選記住", Toast.LENGTH_LONG).show();
+        if (isCheck.isChecked()) {
+            infoSp.edit().putString("name", name)
+                    .putString("password", password)
+                    .putBoolean("check", true)
+                    .apply();
+            Toast.makeText(MainActivity.this, "保存成功" , Toast.LENGTH_SHORT).show();
+        } else {infoSp.edit().putString("name", "")
+                .putString("password", "")
+                .putBoolean("check", false)
+                .apply();
         }
     }
+
 }
 
